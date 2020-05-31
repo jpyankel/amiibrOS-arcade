dd50940f94601ca0485a45963c2086bc5d588cfe
Joseph <jpyankel@gmail.com>
2020-05-31 15:08:34 -0400

    Implement Basic Button Mapping Configuration

--- install/src/inputmanager.py
+++ install/src/inputmanager.py
@@ -4,7 +4,7 @@ import configparser
 
 root_path = (Path(__file__).parent.parent).resolve()
 data_path = root_path / "data"
-controller_path = data_path / "controller.config"
+controller_path = data_path / "gamepads.config"
 
 # tolerance before abs() of any joystick axis value is interpreted as 1 or -1
 analog_tolerance = 0.5
@@ -20,8 +20,8 @@ class InputManager:
       "Y" : False,
       "Se" : False,
       "St" : False,
-      "Ve" : 0,
-      "Ho" : 0
+      "V1" : 0,
+      "H1" : 0
     }
 
     # the first joystick ID that inputs an action gets to control the interface
@@ -31,25 +31,14 @@ class InputManager:
     self.set_primary = True
 
     # load joystick config files from data/controllers.config
-    self.gamepadConfigs = configparser.ConfigParser().read(str(controller_path))
+    self.gamepadConfigs = configparser.ConfigParser()
+    self.gamepadConfigs.read(str(controller_path))
 
     self.joysticks = []
 
     self.refreshJoysticks()
 
   def update(self, dt):
-    if self.set_primary:
-      print("FINDING JOYSTICKS")
-      # Poll any event for the first joystick
-      for joystick in self.joysticks:
-        if joystickAnyInput(joystick):
-          self.primaryID = joystick.get_id()
-          self.set_primary = False
-          print("JOYSTICK FOUND: ", self.joysticks[self.primaryID].get_name())
-    elif self.primary_id != None:
-      # Update currently pressed stuff
-      pass
-
     # TODO replace with pygame.event.pump() when we reimplement the below functionality
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -58,6 +47,19 @@ class InputManager:
       if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_ESCAPE:
           pygame.quit() # TODO REMOVE
+
+    if self.set_primary:
+      print("FINDING JOYSTICKS")
+      # Poll any event for the first joystick
+      for joystick in self.joysticks:
+        if joystickAnyInput(joystick):
+          self.primary_id = joystick.get_id()
+          self.set_primary = False
+          print("JOYSTICK FOUND:")
+          print(self.joysticks[self.primary_id].get_name())
+    elif self.primary_id != None:
+      # Update currently pressed buttons
+      self.updatePrimary()
 
   def refreshJoysticks(self):
     # the primary controller will need to be chosen again
@@ -80,6 +82,25 @@ class InputManager:
 
   def hasPrimary (self):
     return self.primary_id != None
+
+  def updatePrimary (self):
+    joystick = self.joysticks[self.primary_id]
+    config = self.gamepadConfigs[joystick.get_name()]
+
+    # check buttons and axis based on configuration
+    for ctrl in self.processedInputs.keys():
+      # if the ctrl a button (not a joystick)
+      ctrl_id = int(config[ctrl])
+
+      if ctrl != "H1" and ctrl != "V1":
+        self.processedInputs[ctrl] = joystick.get_button(ctrl_id)
+      else:
+        if joystick.get_axis(ctrl_id) > analog_tolerance:
+          self.processedInputs[ctrl] = 1
+        elif joystick.get_axis(ctrl_id) < -analog_tolerance:
+          self.processedInputs[ctrl] = -1
+        else:
+          self.processedInputs[ctrl] = 0
 
 def joystickAnyInput(joystick):
   """ Returns True if any joystick button or other input is not 0 """
